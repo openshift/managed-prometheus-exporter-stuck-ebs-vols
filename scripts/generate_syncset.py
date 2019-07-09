17a421ce08983b9eb0352a714feb03edf1db8297
Naveen Malik <jewzaam@gmail.com>
2019-07-09 11:24:56 -0400

    Update scripts/generate_syncset.py

--- scripts/generate_syncset.py
+++ scripts/generate_syncset.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
         if filenames:
             sss_name = dirpath.replace('/','-')
             if sss_name == arguments.yaml_directory:
-                # files in the root dir, use repo-name for SSS name
+                # files in the root dir, use repo-name for SelectorSyncSet (SSS) name
                 sss_name = arguments.repo_name
             else:
                 # SSS name is based on dirpath which has the root path prefixed.. remove that prefix
